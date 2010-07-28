793980dde308a4648bff33faff2fd69ba6a74a71
Väinö Järvelä <vaino.jarvela@gmail.com>
2010-07-28 22:39:48 +0300

    Refactoring and commenting
    Lot's of moving code around to be a bit more readable.  Also made some
    functions private that shouldn't be of any use for end users.  Some of
    the code got optimized, and maybe a bit easier to read (in my opinion);
    but *every-other* was just a useless excerise in optimization as it will
    never have a lot of data to work on.

--- src/pour/core.clj
+++ src/pour/core.clj
@@ -1,11 +1,26 @@
 (ns pour.core
   (:use [pour.assert-args :only [assert-args]]))
 
+;;; Short circuiting fns
 (def stop ::stop)
 (defn stop-with-value [v] [stop v])
-(defn stop? [v] (= stop (first v)))
-(defn stop-value [v] (second v))
+(defn stop? [s] (= stop (first s)))
+(defn stop-value [s] (second s))
 
+;;; Accessors and predicates for defform results
+(def errors :errors)
+(def values :values)
+(defn errors? [results] (not (empty? (errors results))))
+
+(defmacro fields
+  "Return a vector of fields defined in a form.
+
+  Field order is not retained"
+  [form]
+  `(:fields (meta (var ~form))))
+
+
+;;; Predicate creation helpers
 (defn pred-to-validator
   "Converts a simple predicate to a validator"
   [pred]
@@ -31,8 +46,11 @@
   [& preds]
   `(fn [param#] (when-let [v# (->> param# ~@preds)] v#)))
 
+
 ;;; TODO: Refactor?
-(defn validate [param forms]
+(defn validate
+  "Validates param against a vector of validator forms"
+  [param forms]
   (if (empty? forms)
     [param nil]
     (let [f (first forms)
@@ -43,39 +61,44 @@
        (nil? v) [nil err]
        :else (recur v (nnext forms))))))
 
-(defn- validator [forms]
+(defn- validator
+  "Returns a quoted anonymous validator function for validator forms"
+  [forms]
   `(fn [param#] (validate param# ~forms)))
 
-(defn form* [forms]
+(defn form*
+  "Convert validator forms to a map of fields and validator functions,
+  where the validator function runs through the whole chain."
+  [forms]
   (apply merge
          (for [[k v] (apply array-map forms)]
            {k (validator v)})))
 
-(defn- result-map [place results]
-  (zipmap (keys results) (map place (vals results))))
+(defn- value-map
+  "Transform defform result map to just contain the values"
+  [results]
+  (into {} (for [[k v] results] {k (first v)})))
 
-(defn value-map [results]
-  (result-map first results))
+(defn- error-map
+  "Transform defform result map to just contain the errors"
+  [results]
+  (into {} (for [[k v] results
+                 :let [err (second v)]
+                 :when (not (nil? err))]
+             {k err})))
 
-(defn error-map [results]
-  (result-map second (remove (fn [[k v]] (nil? (second v))) results)))
+(defn transform-results
+  "Transforms defform result map to a more usable format"
+  [results]
+  {values (value-map results) errors (error-map results)})
 
-(def errors :errors)
-(def values :values)
-(defn errors? [results] (not (empty? (errors results))))
-
-(defn transform-results [results]
-  (assoc {values (value-map results)} errors (error-map results)))
-
-(defn- every-other [f coll]
-  (keep-indexed (fn [i v] (if (f i) v)) coll))
-
-(defmacro fields
-  "Return a vector of fields defined in a form.
-
-  Field order is not retained"
-  [form]
-  `(:fields (meta (var ~form))))
+(defn- every-other
+  "Returns a vector with every other value of coll retained"
+  [coll]
+  (loop [coll coll
+         acc []]
+    (if (empty? coll) acc
+        (recur (nnext coll) (conj acc (first coll))))))
 
 (defmacro defform
   "Define a web form for validation"
@@ -83,8 +106,8 @@
   (assert-args
    defform
    (even? (count forms)) "even count for forms vector"
-   (every? keyword? (every-other even? forms)) "keys must be keywords"
-   (every? vector? (every-other odd? forms)) "values must be vectors")
+   (every? keyword? (every-other forms)) "keys must be keywords"
+   (every? vector? (every-other (next forms))) "values must be vectors")
   (let [validators (form* forms)
         fields (set (keys validators))]
     `(defn ~(with-meta name (assoc (meta name) :fields fields))
