6b342ea5560f9bda36e235bfe56edf03e9d9d185
Väinö Järvelä <vaino.jarvela@gmail.com>
2010-07-29 08:46:52 +0300

    pour 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject pour "0.1.0-SNAPSHOT"
+(defproject pour "0.1.0"
   :description "Web form validation library for Clojure"
   :url "http://github.com/Kaali/pour"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-clojars "0.5.0"]])
